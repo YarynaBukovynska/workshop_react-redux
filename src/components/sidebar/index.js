e1572a102000ebc05d0b5dd82477973d64ab9b60
YarynaBukovynska <yaryna.bukovynska@gmail.com>
2020-10-28 11:08:36 +0200

    added utils

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,20 +1,15 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import DatePicker from "react-datepicker";
 import { GrFormNext } from 'react-icons/gr';
 import { GrFormPrevious } from 'react-icons/gr';
 import Table from './Table';
 import './styles.css';
 import "react-datepicker/dist/react-datepicker.css";
+import { formattingDay } from '../../utils'; // use this for calendar data
 
 const Sidebar = () => {
   const [expanded, setExpanded] = useState(false);
   const [date, setDate] = useState(new Date());
-  const formattedDay = useMemo(() => {
-    const dateObj = new Date(date);
-    const day = dateObj.getDate() >= 10 ? dateObj.getDate() : `0${dateObj.getDate()}`;
-    const month = dateObj.getMonth() >= 9 ? dateObj.getMonth() + 1 : `0${dateObj.getMonth() + 1}`;
-    return `${dateObj.getFullYear()}-${month}-${day}`
-  }, [date]);
   return (
     <aside style={{
       width: expanded ? '40%' : '50px'
