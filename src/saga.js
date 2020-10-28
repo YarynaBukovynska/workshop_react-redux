a16189c14e55cb62b9f5e58f080f77b407408e0b
YarynaBukovynska <yaryna.bukovynska@gmail.com>
2020-10-28 19:25:24 +0200

    saga

--- src/saga.js
+++ src/saga.js
@@ -1,10 +1,11 @@
 import { put, call, takeLatest } from 'redux-saga/effects';
 
 // worker Saga: will be fired on SOME_ACTION action
-export function* someSaga() {
-
+export function* someSaga({ data }) {
+  const markers = yield call(getData, data);
+  yield put({ type: SET_MARKERS, data: markers });
 }
 
 export function* saga() {
-  // yield takeLatest(SOME_ACTION, someSaga);
+  yield takeLatest(LOAD_DATA, someSaga);
 }
